6e6a3e819d0f4b4d8692f7bdeaea3226d45d28af
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2024-01-03 20:26:12 -0300

    feat: creation list-thought

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,16 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NewThoughtComponent } from './components/thougths/new-thought/new-thought.component';
 import { FormsModule } from '@angular/forms';
+import { ListThoughtComponent } from './components/thougths/list-thought/list-thought.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NewThoughtComponent
+    NewThoughtComponent,
+    ListThoughtComponent,
   ],
   imports: [
     BrowserModule,
