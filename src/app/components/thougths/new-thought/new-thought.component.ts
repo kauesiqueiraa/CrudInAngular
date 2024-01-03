dee101d2c782baaa1f20c137eb9f7059b7301839
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2024-01-03 20:03:56 -0300

    feat: association with Event Binding and Property binding

---
+++ src/app/components/thougths/new-thought/new-thought.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-thought',
+  templateUrl: './new-thought.component.html',
+  styleUrls: ['./new-thought.component.css']
+})
+export class NewThoughtComponent implements OnInit {
+
+  thought = {
+    id: '1',
+    content: "Aprendendo Angular",
+    authorship: "Dev",
+    model: "",
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  createThought(){
+    alert("new Thought create")
+  }
+
+  cancelThought() {
+    alert("Thought canceled")
+  }
+}
