7fcba8eacfde5f7b3ba0ada4fe41c0684d867811
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2024-01-03 20:32:22 -0300

    feat: creation Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewThoughtComponent } from './components/thougths/new-thought/new-thought.component';
+import { ListThoughtComponent } from './components/thougths/list-thought/list-thought.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "listThought"
+  },
+  {
+    path: "newThought",
+    component: NewThoughtComponent
+  },
+  {
+    path: "listThought",
+    component: ListThoughtComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
